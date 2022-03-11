486555e8e7b84ccb172342af44914a94bb47c1c9
KyleA <arbourkyle@gmail.com>
2022-03-10 20:31:36 -0500

    fixing queries in roles

---
+++ index.js
@@ -0,0 +1,265 @@
+const mysql = require('mysql2');
+const table = ('console.table');
+const inquirer = require('inquirer');
+
+
+/* const viewAllDep = require('./questions');
+const viewAllRole = require('./questions');
+const viewAllEmp = require('./questions');
+const addDep = require('./questions');
+const addRole = require('./questions');
+const addEmp = require('./questions');
+const updateEmpRole = require('./questions'); */
+
+
+const connection = require('./server');
+
+//MAIN PROMPT
+function main() {
+	inquirer
+	  .prompt([
+		{
+			loop: false,
+			type: 'list',
+			name: 'choices',
+			message: 'What would you like to do?',
+			choices: [
+					'view all departments',
+					'view all roles',
+					'view all employees',
+					'add a department',
+					'add a role',
+					'add an employee',
+					'update an employee role',
+					'exit'
+			]
+		}
+])
+.then((answers) => {
+	switch(answers.choices) {
+		case 'view all departments':
+			viewAllDep();
+			console.log(answers)
+			break;
+		case 'view all roles':
+			viewAllRole();
+			break;
+		case 'view all employees':
+			viewAllEmp();
+			break;
+		case 'add a department':
+			addDep()
+			return
+		case 'add a role':
+			addRole();
+			return
+		case 'add an employee':
+			addEmp();
+			return
+		case 'update an employee role':
+			updateEmpRole();
+			return
+		default:
+			connection.end();
+	}
+})//END OF DOT THEN
+
+.catch((error) => {
+		if (error.isTtyError) {
+			console.log('catch if',error)
+	
+		} else {
+			console.log('catch else', error);
+		}
+});
+
+
+
+
+};//END OF MAIN
+main();
+
+
+//DEPARTMENTS TABLE
+const viewAllDep = () => {
+	connection.execute(
+		'SELECT * FROM departments',
+		
+		function(err, results, fields) {
+						
+			console.table(results);
+			main()
+		});
+};
+
+//ROLES TABLE
+const viewAllRole = () => {
+	connection.execute(
+	  'SELECT * FROM roles',
+	  
+	function(err, results, fields) {
+		
+		console.table(results);
+		main()
+	});
+};
+
+//EMPLOYEES TABLE
+const viewAllEmp = () => {
+	connection.execute(
+	  'SELECT * FROM employees',
+	  
+	function(err, results, fields) {
+		
+		console.table(results);
+		main()
+	});
+};
+
+//ADD DEPARTMENT
+const addDep = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'depName',
+				message: 'What is the name of the department?'
+			}
+		])
+		.then((answer) => {
+			connection.execute(
+				'INSERT INTO departments (name) VALUES ("'+answer.depName+'")',
+				
+				function(err, results) {
+					
+					console.table('Added '+answer.depName+' to the database')
+					main()
+				});
+		});
+};
+
+//ADD ROLE
+const addRole = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'roleName',
+				message: 'What is the name of the role?'
+			},
+			{
+				type: 'input',
+				name: 'salary',
+				message: 'What is the salary of the role?'
+			},
+			{
+				type: 'input',
+				name: 'department',
+				message: 'What is the department for the role?'
+			},
+		])
+		.then((answer) => {
+					
+			connection.execute('SELECT * FROM departments',(err, results, fields) =>{depLength = results.length;
+				
+			let title = answer.roleName;
+			let salary = answer.salary;
+			let dep = answer.department;
+			if(answer.department != 'Salesperson' || 'Software Engineer' || 'Accountant' || 'Lawyer') {
+				depLength++;
+			}
+	
+			connection.execute(
+				
+				'INSERT INTO roles (title, salary) VALUES ("'+title+'","'+salary+'")',
+				
+				(err, results) => {console.table(results);
+					
+					// main()
+			});
+			connection.execute(
+				
+				'INSERT INTO departments (name) VALUES ("'+dep+'")',
+				
+				(err, results) => {console.table(results);
+					
+					// main()
+			});
+			
+		});			
+	});
+};
+
+//ADD EMPLOYEE
+const addEmp = ()=>{
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'firstName',
+				message: 'What is the employee\'s first name?'
+			},
+			{
+				type: 'input',
+				name: 'lastName',
+				message: 'What is the employee\'s last name?'
+			},
+			{
+				type: 'list',
+				name: 'roleName',
+				message: 'What is the employee\'s role?',
+				choices: [
+					'Salesperson',
+					'Software Engineer',
+					'Accountant',
+					'Lawyer'
+				],
+			}
+		])
+		.then((answer) => {
+			var role;
+			var manager;
+			if(answer.roleName == 'Salesperson'){
+				role = 2;
+				manager = 1; 
+			}
+			else if(answer.roleName == 'Software Engineer'){
+				role = 4;
+				manager = 3;
+			}
+			else if(answer.roleName == 'Accountant'){
+				role = 6;
+				manager = 5;
+			}
+			else {
+				role = 8;
+				manager = 7
+			}
+	
+			connection.query(
+				'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("'+answer.firstName+'","'+ answer.lastName+'","'+role+'","'+manager+'")',
+				
+				function(err, results) {
+					console.table([
+						{
+						first_name: answer.firstName,
+						last_name: answer.lastName,
+						role_id: role,
+						manager_id: manager
+						}
+						
+				]);
+			main()
+		});
+	});
+};
+
+//UPDATE ROLE
+const updateEmpRole = () => {
+connection.execute(
+	'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (Fred, Zed, 2, 1)',
+	
+	function(err, results, empName) {
+		console.log(results)
+});
+}
