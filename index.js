bb25bad7a53681ec39a3c8f2c419437c842a7a5f
KyleA <arbourkyle@gmail.com>
2022-03-13 17:48:08 -0400

    hardcoded names instead, added dot then, trying to insert into employee table

--- index.js
+++ index.js
@@ -15,25 +15,25 @@ const updateEmpRole = require('./questions'); */
 
 
 console.log(figlet.textSync('Employee \n Manager', {
-    font: 'Crawford2',
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-    width: 80,
-    whitespaceBreak: true
+	font: 'Crawford2',
+	horizontalLayout: 'default',
+	verticalLayout: 'default',
+	width: 80,
+	whitespaceBreak: true
 
 }));
-                                                 
-                                                 
+
+
 //MAIN PROMPT
 function main() {
 	inquirer
-	  .prompt([
-		{
-			loop: false,
-			type: 'list',
-			name: 'choices',
-			message: 'What would you like to do?',
-			choices: [
+		.prompt([
+			{
+				loop: false,
+				type: 'list',
+				name: 'choices',
+				message: 'What would you like to do?',
+				choices: [
 					'view all departments',
 					'view all roles',
 					'view all employees',
@@ -42,46 +42,46 @@ function main() {
 					'add an employee',
 					'update an employee role',
 					'exit'
-			]
-		}
-])
-.then((answers) => {
-	switch(answers.choices) {
-		case 'view all departments':
-			viewAllDep();
-			console.log(answers)
-			break;
-		case 'view all roles':
-			viewAllRole();
-			break;
-		case 'view all employees':
-			viewAllEmp();
-			break;
-		case 'add a department':
-			addDep()
-			return
-		case 'add a role':
-			addRole();
-			return
-		case 'add an employee':
-			addEmp();
-			return
-		case 'update an employee role':
-			updateEmpRole();
-			return
-		default:
-			connection.end();
-	}
-})//END OF DOT THEN
+				]
+			}
+		])
+		.then((answers) => {
+			switch (answers.choices) {
+				case 'view all departments':
+					viewAllDep();
+					console.log(answers)
+					break;
+				case 'view all roles':
+					viewAllRole();
+					break;
+				case 'view all employees':
+					viewAllEmp();
+					break;
+				case 'add a department':
+					addDep()
+					return
+				case 'add a role':
+					addRole();
+					return
+				case 'add an employee':
+					addEmp();
+					return
+				case 'update an employee role':
+					updateEmpRole();
+					return
+				default:
+					connection.end();
+			}
+		})//END OF DOT THEN
 
-.catch((error) => {
-		if (error.isTtyError) {
-			console.log('catch if',error)
-	
-		} else {
-			console.log('catch else', error);
-		}
-});
+		.catch((error) => {
+			if (error.isTtyError) {
+				console.log('catch if', error)
+
+			} else {
+				console.log('catch else', error);
+			}
+		});
 
 
 
@@ -94,9 +94,9 @@ main();
 const viewAllDep = () => {
 	connection.execute(
 		'SELECT * FROM departments',
-		
-		function(err, results, fields) {
-						
+
+		function (err, results, fields) {
+
 			console.table(results);
 			main()
 		});
@@ -105,25 +105,25 @@ const viewAllDep = () => {
 //ROLES TABLE
 const viewAllRole = () => {
 	connection.execute(
-	  'SELECT * FROM roles',
-	  
-	function(err, results, fields) {
-		
-		console.table(results);
-		main()
-	});
+		'SELECT * FROM roles',
+
+		function (err, results, fields) {
+
+			console.table(results);
+			main()
+		});
 };
 
 //EMPLOYEES TABLE
 const viewAllEmp = () => {
 	connection.execute(
-	  'SELECT * FROM employees',
-	  
-	function(err, results, fields) {
-		
-		console.table(results);
-		main()
-	});
+		'SELECT * FROM employees',
+
+		function (err, results, fields) {
+
+			console.table(results);
+			main()
+		});
 };
 
 //ADD DEPARTMENT
@@ -138,11 +138,11 @@ const addDep = () => {
 		])
 		.then((answer) => {
 			connection.execute(
-				'INSERT INTO departments (name) VALUES ("'+answer.depName+'")',
-				
-				function(err, results) {
-					
-					console.table('Added '+answer.depName+' to the database')
+				'INSERT INTO departments (name) VALUES ("' + answer.depName + '")',
+
+				function (err, results) {
+
+					console.table('Added ' + answer.depName + ' to the database')
 					main()
 				});
 		});
@@ -172,39 +172,42 @@ const addRole = () => {
 		//return this.conncetion.promise().query("INSERT INTO name SET ?", name);
 
 		.then((answer) => {
-					
-			connection.execute('SELECT * FROM departments',(err, results, fields) =>{depLength = results.length;
-				
-			let title = answer.roleName;
-			let salary = answer.salary;
-			let dep = answer.department;
-			if(answer.department != 'Salesperson' || 'Software Engineer' || 'Accountant' || 'Lawyer') {
-				depLength++;
-			}
-	
-			connection.execute(
-				
-				'INSERT INTO departments (name) VALUES ("'+dep+'")',
-				
-				(err, results) => {console.table('added "'+dep+'" to roles table');
-					
-					// main()
+
+			connection.execute('SELECT * FROM departments', (err, results, fields) => {
+				depLength = results.length;
+
+				let title = answer.roleName;
+				let salary = answer.salary;
+				let dep = answer.department;
+				if (answer.department != 'Salesperson' || 'Software Engineer' || 'Accountant' || 'Lawyer') {
+					depLength++;
+				}
+
+				connection.execute(
+
+					'INSERT INTO departments (name) VALUES ("' + dep + '")',
+
+					(err, results) => {
+						console.table('added "' + dep + '" to roles table');
+
+						// main()
+					});
+				connection.execute(
+
+					'INSERT INTO roles (title, salary, department_id) VALUES ("' + title + '","' + salary + '","' + depLength + '")',
+
+					(err, results) => {
+						console.table('added "' + title + '","' + salary + '" to roles table');
+
+						main()
+					});
+
 			});
-			connection.execute(
-				//HOW DO I ADD DEPLENGTH TO DEPARTMENT_ID ????????????????????????????????????????????????????????
-				'INSERT INTO roles (title, salary, department_id) VALUES ("'+title+'","'+salary+'","'+depLength+'")',
-				
-				(err, results) => {console.table('added "'+title+'","'+salary+'" to roles table');
-					
-					main()
-			});
-			
-		});			
-	});
+		});
 };
 
 //ADD EMPLOYEE
-const addEmp = ()=>{
+const addEmp = () => {
 	inquirer
 		.prompt([
 			{
@@ -232,15 +235,15 @@ const addEmp = ()=>{
 		.then((answer) => {
 			var role;
 			var manager;
-			if(answer.roleName == 'Salesperson'){
+			if (answer.roleName == 'Salesperson') {
 				role = 2;
-				manager = 1; 
+				manager = 1;
 			}
-			else if(answer.roleName == 'Software Engineer'){
+			else if (answer.roleName == 'Software Engineer') {
 				role = 4;
 				manager = 3;
 			}
-			else if(answer.roleName == 'Accountant'){
+			else if (answer.roleName == 'Accountant') {
 				role = 6;
 				manager = 5;
 			}
@@ -248,65 +251,146 @@ const addEmp = ()=>{
 				role = 8;
 				manager = 7
 			}
-	
+
 			connection.query(
-				'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("'+answer.firstName+'","'+ answer.lastName+'","'+role+'","'+manager+'")',
-				
-				function(err, results) {
+				'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("' + answer.firstName + '","' + answer.lastName + '","' + role + '","' + manager + '")',
+
+				function (err, results) {
 					console.table([
 						{
-						first_name: answer.firstName,
-						last_name: answer.lastName,
-						role_id: role,
-						manager_id: manager
-					}
-					
-				]);
-				main()
-			});
+							first_name: answer.firstName,
+							last_name: answer.lastName,
+							role_id: role,
+							manager_id: manager
+						}
+
+					]);
+					main()
+				});
 		});
-	};
-	
-	//UPDATE ROLE
-	const updateEmpRole = () => {
-		
-		first = [];
-		last = [];
-		connection.execute('SELECT first_name, last_name FROM employees', (err, results) => {
-			results.forEach(element => {
-				
-				first = element.first_name;
-				last = element.last_name;
-			});
+};
+
+//UPDATE ROLE
+const updateEmpRole = () => {
+
+	connection.execute('SELECT first_name, last_name FROM employees', (err, results) => {
+
+
+		const iterator = results.values();
+		var first = [];
+		var last = [];
+		var fullName = [];
+		var lastName = [];
+		for (const value of iterator) {
+
+			first.push(value.first_name);
+			last.push(value.last_name);
 			
+			fullName.push(value.first_name + " " + value.last_name)
+			// console.log('', fullName)
+			lastName.push(fullName[fullName.length - 1])
+			// console.log('last',last)
+
+		}
+		if (fullName[fullName.length] !== fullName[fullName.length - 1]) {
+		}
+
+		console.log('full',fullName)
+		console.log('first', first)
+		console.log('last', last)
+		// console.log(fullName[fullName.length-1])
+		// results.forEach(element => {
+		// 	let name = element.first_name+" "+element.last_name;
+
+		// console.log('names', fullName.length)
+
+		connection.execute('SELECT title FROM roles', (err, results) => {
 			
-			connection.execute('SELECT title FROM roles', (err, results) => {let roles = results;
-				console.log('inEach',first);
-				console.log('inEach',last);
+			console.log(results[0].title)
+			
+			inquirer.prompt([
+				{
+					type: 'list',
+					name: 'empName',
+					message: 'Which employee\'s role do you want to update?',
+					choices: [
+						{ name: fullName[0], value: first[0]+' '+last[0] },
+						{ name: fullName[1], value: first[1]+' '+last[1] },
+						{ name: fullName[2], value: fullName[2] },
+						{ name: fullName[fullName.length - 1], value: fullName[fullName.length - 1] },
+					]
+
+				},
+				{
+					type: 'list',
+					name: 'roleName',
+					message: 'What is the employee\'s role?',
+					choices: [
+							{ name: results[0].title, value: results[0].title },
+							{ name: results[1].title, value: results[1].title },
+							{ name: results[2].title, value: results[2].title },
+							{ name: results[3].title, value: results[3].title },
+				]
+				},
+			])
+			.then((answers) => {
+				let spacer = ' ';
+				let split = answers.empName;
+				let firstName =split.split(0, ' '); let lastName =split.split(' ', -1)
+				// console.log(lastName[0])
+
 				
-				inquirer.prompt([
-					{
-						type: 'list',
-						name: 'roleName',
-						message: 'Which employee\'s role do you want to update?',
-						choices: [[`${first}`][`${last}`]]
-					},
-					{
-						type: 'list',
-						name: 'roleName',
-						message: 'What is the employee\'s role?',
-						choices: [roles]
-					},
-				])
+				if(answers.roleName == 'Sales Lead') {
+					role_id = 1;
+					manager_id = null;
+				}
+				else if(answers.roleName == 'Salesperson') {
+					role_id = 2;
+					manager_id = 1;
+				}
+				else if(answers.roleName == 'Lead Engineer') {
+					role_id = 3;
+					manager_id = null;
+				}
+				else if(answers.roleName == 'Software Engineer') {
+					role_id = 4;
+					manager_id = 3;
+				}
+				else if(answers.roleName == 'Accountant Manager') {
+					role_id = 5;
+					manager_id = null;
+				}
+				else if(answers.roleName == 'Accountant') {
+					role_id = 6;
+					manager_id = 5;
+				}
+				else if(answers.roleName == 'Legal Team Lead') {
+					role_id = 7;
+					manager_id = null;
+				}
+				else if(answers.roleName == 'Lawyer') {
+					role_id = 8;
+					manager_id = 7;
+				}
+				else{
+					role_id = roles.length++;
+					manager_id = null;
+				}
 				
-				connection.execute(
-					
-					'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (Fred, Zed, 2, 1)',
-					
-					function(err, results) {
-						console.log('insert emp',results)
+				
+
+			connection.execute(
+				 
+				 'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (lastName[0], lastName[1], role_id, manager_id)',
+
+				function (err, results) {
+					// console.log(err)
+				
+				})//END OF THEN
+				//}); //END OF FOREACH
+			
 			});//END OF INSERT INTO EMPLOYEES
+		
 		});//END OF SELECT NAME FROM EMPLOYEES
 	});//END OF SELECT TITLE FROM ROLES
-}
-		
+}//END OF UPDATE EMPLOYEE FUNC
